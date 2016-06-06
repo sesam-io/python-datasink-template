b15c40683bb6e05bec4e1e66665ac4ca32232180
Geir Ove Grønmo <grove@geirove.org>
2016-06-06 07:55:47 +0200

    Writing output files to ./received directory

--- service/datasink-service.py
+++ service/datasink-service.py
@@ -6,13 +6,20 @@ app = Flask(__name__)
 
 @app.route('/receiver', methods=['POST'])
 def receiver():
+    # create output directory
+    directory = os.path.join(os.getcwd(), "received")
+    os.makedirs(directory, exists_ok=True)
+
+    # get entities from request and write each of them to a file
     entities = request.get_json()
     for entity in entities:
         entity_id = entity["_id"]
-        filename = os.path.join(os.getcwd(), entity_id + ".json")
+        filename = os.path.join(directory, entity_id + ".json")
         print("Writing entity \"%s\" to file '%s'" % (entity_id, filename))
         with open(filename, "w") as fp:
             json.dump(entity, fp, indent=4, sort_keys=True)
+
+    # create the response
     return Response("Thanks!", mimetype='text/plain')
 
 if __name__ == '__main__':
