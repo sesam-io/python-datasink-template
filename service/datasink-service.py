0670640ba5ce65ef7b7bd219898d66b9fede3f3b
Tom Bech <tom.bech@gmail.com>
2019-05-09 13:04:00 +0200

    Extended template with logging example

--- service/datasink-service.py
+++ service/datasink-service.py
@@ -2,8 +2,19 @@ from flask import Flask, request, Response
 import cherrypy
 import json
 import os
+import logging
+import paste.translogger
+
 
 app = Flask(__name__)
+
+logger = logging.getLogger("datasink-service")
+
+
+@app.route('/', methods=['GET'])
+def root():
+    return Response(status=200, response="I am Groot!")
+
 
 @app.route('/receiver', methods=['POST'])
 def receiver():
@@ -19,14 +30,30 @@ def receiver():
     for entity in entities:
         entity_id = entity["_id"]
         filename = os.path.join(directory, entity_id + ".json")
-        print("Writing entity \"%s\" to file '%s'" % (entity_id, filename))
+        logger.info("Writing entity \"%s\" to file '%s'" % (entity_id, filename))
         with open(filename, "w") as fp:
             json.dump(entity, fp, indent=4, sort_keys=True)
 
     # create the response
     return Response("Thanks!", mimetype='text/plain')
 
+
 if __name__ == '__main__':
+    format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+    # Log to stdout, change to or add a (Rotating)FileHandler to log to a file
+    stdout_handler = logging.StreamHandler()
+    stdout_handler.setFormatter(logging.Formatter(format_string))
+    logger.addHandler(stdout_handler)
+
+    # Comment these two lines if you don't want access request logging
+    app.wsgi_app = paste.translogger.TransLogger(app.wsgi_app, logger_name=logger.name,
+                                                 setup_console_handler=False)
+    app.logger.addHandler(stdout_handler)
+
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+
     cherrypy.tree.graft(app, '/')
 
     # Set the configuration of the web server to production mode
